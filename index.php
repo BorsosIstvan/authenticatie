d5dbd3452ab81dfeaea8586aa5bc007ee1369159
BorsosIstvan <bistvanus@gmail.com>
2025-07-13 11:45:27 +0200

    style.css

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
 session_start();
-
-if (isset($_SESSION['username'])) {
-    echo "<h2>Welkom, " . htmlspecialchars($_SESSION['username']) . "!</h2>";
-    echo '<a href="logout.php">Uitloggen</a>';
-} else {
-    echo "<h2>Je bent niet ingelogd.</h2>";
-    echo '<a href="login.html">Inloggen</a> | <a href="register.html">Aanmelden</a>';
+if (!isset($_SESSION['username'])) {
+    header('Location: login.html');
+    exit;
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Welkom</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welkom, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
+        <p>Je bent succesvol ingelogd.</p>
+        <a href="logout.php" class="button">Uitloggen</a>
+    </div>
+</body>
+</html>
